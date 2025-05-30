fd09792fd01ed8fc146cd722824605d5b3577112
Kevin Nerio <neriokevin1@gmail.com>
2025-05-30 15:52:09 -0700

    fix: forgot interface.

--- online-portfolio/app/components/homepage/about.tsx
+++ online-portfolio/app/components/homepage/about.tsx
@@ -1,6 +1,11 @@
 "use client";
 import { motion } from "framer-motion";
 
+interface AboutData{
+  title: string; 
+  summary: string; 
+}
+
 export default function AboutHome({ about_data } : { about_data: AboutData }) {
 
   return (
@@ -14,7 +19,7 @@ export default function AboutHome({ about_data } : { about_data: AboutData }) {
           transition={{ duration: 0.8 }}
           className="flex flex-col items-center text-center w-full bg-center shadow-lg border backdrop-blur-md bg-gray-100 border border-white/10 p-6 max-w-5xl outline outline-2 outline-white/20 outline-offset-4"
         >
-          <h1 className="text-gray-800 text-4xl font-semibold flex items-center justify-center py-6">About Section</h1>
+          <h1 className="text-gray-800 text-4xl font-semibold flex items-center justify-center py-6">{about_data.title}</h1>
           <div className="flex items-center justify-center py-8 ">
             <div className="gap-8 px-6 sm:px-12 max-w-5xl w-full">
               <p>Design, develop, code, and troubleshoot with consideration of upstream and downstream systems and technical implications. Apply knowledge of tools within the Software Development Life Cycle toolchain to improve the value realized by automation. Participate in code reviews to ensure solutions align with predefined specifications. </p>
